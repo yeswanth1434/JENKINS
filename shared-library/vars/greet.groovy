5c7d8f7493b47467c8bc2bd7a1eaabad1a031aa7
yeswanth1434 <119117435+yeswanth1434@users.noreply.github.com>
2023-01-29 20:11:49 +0530

    Update greet.groovy

--- shared-library/vars/greet.groovy
+++ shared-library/vars/greet.groovy
@@ -1,3 +1,19 @@
 def call (name) {
   echo "Hi ${name},welcome"
 }
+
+
+in jenkins pipeline :
+
+ @Library('sharelibs') _
+
+ pipeline {
+   agent any
+ 	    stages {
+          stage('Hello') {
+             steps {
+                 greet('yeswanth')
+             }
+          }
+      }
+ }
