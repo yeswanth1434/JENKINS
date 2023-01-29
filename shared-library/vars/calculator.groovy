b4e3a0f7a66d499ec60dd3e6b296b937601671d4
yeswanth1434 <119117435+yeswanth1434@users.noreply.github.com>
2023-01-29 20:06:29 +0530

    Update calculator.groovy

--- shared-library/vars/calculator.groovy
+++ shared-library/vars/calculator.groovy
@@ -4,3 +4,24 @@ def add(x,y) {
 def mul(x,y) {
   echo " sum of ${x} and ${y} is ${x*y}"
 }
+
+
+
+
+in jenkins pipeline:
+@Library('sharelibs') _
+
+pipeline {
+    agent any
+
+    stages {
+        stage('add') {
+            steps {
+                script {
+                    calculator.add(22,33)
+                    calculator.mul(7,8)
+                }
+            }
+        }
+    }
+}
